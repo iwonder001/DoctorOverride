ac16c93ea633ccdc0320f35eaaf665ca9945ae08
Kristen Carethers <kcarethers@gmail.com>
2016-10-03 15:57:18 -0400

    Initial commit

---
+++ src/com/javaship/Doctor.java
@@ -0,0 +1,12 @@
+package com.javaship;
+
+public class Doctor {
+	// the instance variable
+	public boolean worksAtHospital;
+
+	// the constructor
+	public Doctor(boolean w) {
+
+	}
+
+}
---
+++ src/com/javaship/FamilyDoctor.java
@@ -0,0 +1,5 @@
+package com.javaship;
+
+public class FamilyDoctor {
+
+}
